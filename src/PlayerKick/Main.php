34a3dc14247569ee54b4d3412060721e2f7658f8
GroupPluginer <4gamesl7o@gmail.com>
2017-07-15 22:26:25 +0300

    Update Main.php

--- src/PlayerKick/Main.php
+++ src/PlayerKick/Main.php
@@ -16,7 +16,6 @@ $this->getServer()->getPluginManager()->registerEvent($this, $this);
 public function onKick(PlayerKickEvent $event){
 $player = $event->getPlayer();
 $name = $event->getName();
-
-$player->setKickMessage(Color::RED."لقد تم طردك من قبل الادمن لعدم تقبل اوامر السيرفر"):
-   }
-  }
+  $player->setKickMessage(Color::RED."لقد تم طردك من قبل الادمن لعدم تقبل اوامر السيرفر"):
+ }
+}
