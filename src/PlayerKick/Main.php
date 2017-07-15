85495f677f46083d19c2262e74ae16a2f587d80e
GroupPluginer <4gamesl7o@gmail.com>
2017-07-15 22:53:32 +0300

    Update Main.php

--- src/PlayerKick/Main.php
+++ src/PlayerKick/Main.php
@@ -10,7 +10,7 @@ use pocketmine\text\TextFormat as Color;
 class Main extends PluginBase implements Listener{
 
 public function onEnable(){
-$this->getServer()->getPluginManager()->registerEvent($this, $this);
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
  }
 
 public function onKick(PlayerKickEvent $event){
